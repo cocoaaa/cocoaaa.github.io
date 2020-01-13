e9b32cd9b50200cee1711da52e19cad04dc508e0
Hayley Song <haejinso@usc.edu>
2020-01-12 16:16:06 -0800

    fixes small errors in publish config

--- publishconf.py
+++ publishconf.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
 
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 
-import os
-import sys
+import os, sys
+import pdb
 sys.path.append(os.curdir)
-from pelicanconf_v1 import *
+from pelicanconf_blue_penguin import *
+print('theme: ', THEME)
 
+# pdb.set_trace()
 SITEURL = 'http://cocoaaa.github.io'
 RELATIVE_URLS = False
 
